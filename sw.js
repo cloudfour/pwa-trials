5669d35d739b14086277a9670ac9eb4b6eab76f2
Erik Jung <erik@cloudfour.com>
2016-09-01 16:19:53 -0700

    Tweak activate handler logic

--- sw.js
+++ sw.js
@@ -7,8 +7,7 @@ const deps = [
   '/assets/main.js'
 ];
 
-addEventListener('install', event => {
-  console.log(`${event.type} ${version}`);
+addEventListener('install', event => { console.log(`${event.type} ${version}`);
   event.waitUntil(
     caches.open(cacheName)
       .then(cache => cache.addAll(deps))
@@ -17,27 +16,27 @@ addEventListener('install', event => {
   );
 });
 
-addEventListener('activate', event => {
-  console.log(`${event.type} ${version}`);
+addEventListener('activate', event => { console.log(`${event.type} ${version}`);
   event.waitUntil(
     caches.keys()
       .then(keys => keys
         .filter(key => !key.startsWith(version))
         .map(key => caches.delete(key))
       )
+      .then(Promise.all)
       .catch(err => console.warn(err))
       .then(clients.claim())
   );
 });
 
-addEventListener('fetch', event => {
-  console.log(event.request);
+addEventListener('fetch', event => { console.log(event.request);
+  //
 });
 
-addEventListener('message', event => {
-  console.log(`${event.type} ${version}`);
+addEventListener('message', event => { console.log(`${event.type} ${version}`);
+  //
 });
 
-addEventListener('sync', event => {
-  console.log(`${event.type} ${version}`);
+addEventListener('sync', event => { console.log(`${event.type} ${version}`);
+  //
 });
