4082365a58376215be5bf1a1d478d6e849c335cd
Erik Jung <erik@cloudfour.com>
2016-09-19 15:27:00 -0700

    Tweak test page JS

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,44 +2,21 @@
 
 if ('serviceWorker' in navigator) {
   const sw = navigator.serviceWorker;
-  const form = document.querySelector('form');
-  const reloadButton = document.getElementById('reload');
 
-  sw.register('/sw.js').then(reg => {
-    console.log(reg);
+  sw.register('/sw.js');
+
+  window.sendFormData = function (form) {
+    const formData = new FormData(form);
+    sw.controller.postMessage(formData.get('msg'));
+  };
+
+  // Debug logging
+
+  ['controllerchange', 'message'].forEach(type => {
+    sw.addEventListener(type, console.log.bind(console));
   });
 
-  sw.addEventListener('controllerchange', event => {
-    console.log(event);
+  ['online', 'offline'].forEach(type => {
+    addEventListener(type, console.log.bind(console));
   });
-
-  sw.addEventListener('message', event => {
-    console.log(event);
-  });
-
-  addEventListener('online', event => {
-    console.log(event);
-  });
-
-  addEventListener('offline', event => {
-    console.log(event);
-  });
-
-  if (form) {
-    form.addEventListener('submit', event => {
-      console.log(event);
-      sw.controller.postMessage(new FormData(event.target).get('msg'));
-      event.preventDefault();
-    });
-  }
-
-  if (reloadButton) {
-    reloadButton.addEventListener('click', event => {
-      Array.from(document.querySelectorAll('link[rel=stylesheet]')).forEach((item, index, arr) => {
-        const el = arr[index];
-        const clone = el.cloneNode();
-        el.replaceWith(clone);
-      });
-    });
-  }
 }
